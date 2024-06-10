8a5574a393629686ca7f3bca80db3e3590d24b71
Odyssey <77610388+Odyssey409@users.noreply.github.com>
2024-06-10 18:23:30 +0900

    section#2.1 Our first styled components
    전반전 프로젝트 파일들과 styled components의 초기 세팅 및 사용방법 소개

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+
+const Father = styled.div`
+  display: flex;
+`;
+
+const Box = styled.div`
+  width: 100px;
+  height: 100px;
+  background-color: teal;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Father>
+      <Box></Box>
+    </Father>
   );
 }
 
