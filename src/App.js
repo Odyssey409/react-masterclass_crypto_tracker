b60f9e96108d1b6e958950444ba278a2dfac58da
Odyssey <77610388+Odyssey409@users.noreply.github.com>
2024-06-11 00:37:03 +0900

    section#2.7 Themes
    ThemeProvider를 활용하여 객체에 대한 속성을 props를 활용하여 부여함으로써 다크모드 및 라이트모드 구현이 가능함을 학습

--- src/App.js
+++ src/App.js
@@ -2,52 +2,21 @@ import styled, { keyframes } from "styled-components";
 
 const Wrapper = styled.div`
   display: flex;
-`;
-
-const rotateAnimation = keyframes`
-  0%{
-    transform:rotate(0deg);
-    border-radius:0px;
-  }
-  50%{
-    transform:rotate(360deg);
-    border-radius:100px;
-  }
-  100%{
-    transform:rotate(0deg);
-    border-radius:0px;
-  }
-`;
-
-const Emoji = styled.span`
-  font-size: 36px;
-`;
-
-const Box = styled.div`
-  height: 100px;
-  width: 100px;
-  background-color: tomato;
-  display: flex;
   justify-content: center;
   align-items: center;
-  animation: ${rotateAnimation} 1s linear infinite;
+  width: 100vw;
+  height: 100vh;
+  background-color: ${(props) => props.theme.backgroundColor};
+`;
 
-  ${Emoji} {
-    &:hover {
-      font-size: 98px;
-    }
-  }
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box>
-        <Emoji>😀</Emoji>
-      </Box>
-      <Emoji>😀</Emoji>
-      {/* 여기 있는 Emoji는 pseudo  selecting에 의해 영향을 받지 않음 
-      Emoji에 대한 pseudo  selecting을 Box하위에 있는 Emoji로 했기 때문*/}
+      <Title>Hello</Title>
     </Wrapper>
   );
 }
