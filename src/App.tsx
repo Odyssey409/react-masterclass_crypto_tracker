1801f8c1c6bbe1641755cef01e2674c09648defe
Odyssey <77610388+Odyssey409@users.noreply.github.com>
2024-06-11 19:46:24 +0900

    section#3.5 Forms
    TypeScript에서 HTML태그인 form의 event parameter어떻게 전달하는지 학습. event 의 타입을 명시해주기 위해서는 인풋의 경우는React.FormEvent<HTMLInputElement> 으로 표시하고, form은  React.FormEvent<HTMLFormElement>으로 전달해야함

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
-import Circle from "./Circle";
+import React, { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setValue(value);
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("hello", value);
+  };
   return (
     <div>
-      <Circle borderColor="yellow" bgColor="tomato"></Circle>
-      <Circle bgColor="teal"></Circle>
+      <form onSubmit={onSubmit}>
+        <input
+          value={value}
+          onChange={onChange}
+          type="text"
+          placeholder="username"
+        />
+        <button>Log In</button>
+      </form>
     </div>
   );
 }
