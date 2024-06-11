fad79bac7be7ec3060f653b76bfa3b805453b38b
Odyssey <77610388+Odyssey409@users.noreply.github.com>
2024-06-11 20:00:22 +0900

    section#3.6 Themes
    TypeScript에서 themeProvider를 사용하여 코드 실수로부터 보호받는 theme만들기 기능 구현 가능.

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,19 @@
 import React, { useState } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 
 function App() {
-  const [value, setValue] = useState("");
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setValue(value);
-  };
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log("hello", value);
-  };
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input
-          value={value}
-          onChange={onChange}
-          type="text"
-          placeholder="username"
-        />
-        <button>Log In</button>
-      </form>
-    </div>
+    <Container>
+      <H1>Hi</H1>
+    </Container>
   );
 }
 
