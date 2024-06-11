43661f0d606e4e33b57f935608a034ffd954b60f
Odyssey <77610388+Odyssey409@users.noreply.github.com>
2024-06-11 19:16:08 +0900

    section#3.4 State
    TypeScript에서 React의 State를 사용하는 방법을 학습. state의 타입을 타입스크립트가 자동으로 인식하는 방식과 둘 중 하나일 수 있다는 걸 알려주는 방식을 학습. 후자의 경우는 보통은 잘 안 쓰지만 알아는 둬야함.

--- src/Circle.tsx
+++ src/Circle.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styled from "styled-components";
 
 interface ContainerProps {
@@ -19,6 +20,8 @@ interface CircleProps {
 }
 
 function Circle({ bgColor, borderColor }: CircleProps) {
+  const [counter, setCounter] = useState(1); // 이 방식으로 하면 Typescript가 자동으로 counter가 number type으로 인식
+  const [value, setValue] = useState<number | string>(0); // 이 방식이면 Typescriot에게 value가 number일 수도 string일수도 있다고 알려주는 것
   return <Container bgColor={bgColor} borderColor={borderColor ?? bgColor} />;
 }
 
