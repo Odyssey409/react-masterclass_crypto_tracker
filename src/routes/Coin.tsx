e331c8933968b28f1bad58133050036f66f1c16e
Odyssey <77610388+Odyssey409@users.noreply.github.com>
2024-06-12 19:02:33 +0900

    section#5.4 States
    유저에게 코인정보를 보여줄 때 이밎를 추가해서 UX경험을 높였고, 코인 상세페이지로 라우팅 될 때 코인 이름을 다시 불러오는 것이 아닌 브라우적사 이미 알고있는 코인 이름을 useLocation이라는 Hook의 State를 활용하여 전달받아 화면에 바로 띄워주는 기능을 구현.
    
    __________________________________
    
    이슈 발생: 라우팅 될 때 페이지 주소는 바뀌는데 화면이 자동으로 넘어가지 않고 주소를 엔터쳐야 넘어감

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,13 +1,51 @@
-import { useParams } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
+import { styled } from "styled-components";
+import { useState } from "react";
 
 interface RouteParams {
   coinId: string;
 }
 
+const Title = styled.h1`
+  font-size: 48px;
+  color: ${(props) => props.theme.accentColor};
+`;
+
+const Loader = styled.span`
+  text-align: center;
+  display: block;
+`;
+
+const Container = styled.div`
+  padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`;
+
+const Header = styled.header`
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+interface RouteState {
+  name: string;
+}
+
 function Coin() {
   const { coinId } = useParams<RouteParams>();
+  const [loading, setLoading] = useState(true);
+  const { state } = useLocation<RouteState>();
 
-  return <h1>Coin: {coinId}</h1>;
+  return (
+    <Container>
+      <Header>
+        <Title>{state?.name || "Loading..."}</Title>
+      </Header>
+      {loading ? <Loader>Loading...</Loader> : null}
+    </Container>
+  );
 }
 
 export default Coin;
