82d1bba9e5eadceb4ff786fbdf759a445473defd
Odyssey <77610388+Odyssey409@users.noreply.github.com>
2024-06-12 19:32:17 +0900

    issue fixed
    이슈 발생: 라우팅 될 때 페이지 주소는 바뀌는데 화면이 자동으로 넘어가지 않고 주소를 엔터쳐야 넘어감
    
    --> 이 문제를 BrowserRouter가 단일로 하나만 감싸야 했다는 사실을 꺠닫고 고쳐 해결
    
    https://kjhg478.tistory.com/26 를 참고하여 해결함

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,16 +4,14 @@ import Coins from "./routes/Coins";
 
 function Router() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/:coinId">
-          <Coin />
-        </Route>
-        <Route path="/">
-          <Coins />
-        </Route>
-      </Switch>
-    </BrowserRouter>
+    <Switch>
+      <Route path="/:coinId">
+        <Coin />
+      </Route>
+      <Route path="/">
+        <Coins />
+      </Route>
+    </Switch>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,17 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./theme";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>
+  <BrowserRouter>
+    <React.StrictMode>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </React.StrictMode>
+  </BrowserRouter>
 );
