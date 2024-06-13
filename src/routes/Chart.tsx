1eb6c30ee71756553a5867093cc648590908b819
Odyssey <77610388+Odyssey409@users.noreply.github.com>
2024-06-13 16:46:04 +0900

    section#5.14 Price Chart part Three
    차트 다듬기 과정. 툴팁에서 표시되는 x축에 날짜가 UTC 기준으로 변환되어 dd mmm 형식으로 뜨게 하는 작업과 가격을 달러 표시 표기 및 소수점 두 자부터 버림 기능 적용

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -76,9 +76,26 @@ function Chart({ coinId }: ChartProps) {
               labels: {
                 show: false,
               },
+              type: "datetime",
+              categories: data?.map((price) =>
+                new Date(price.time_close * 1000).toUTCString()
+              ),
             },
             yaxis: {
               show: false,
+            },
+            fill: {
+              type: "gradient",
+              gradient: {
+                gradientToColors: ["#0be881"],
+                stops: [0, 100],
+              },
+            },
+            colors: ["#0fbcf9"],
+            tooltip: {
+              y: {
+                formatter: (value) => `$ ${value.toFixed(2)}`,
+              },
             },
           }}
         />
