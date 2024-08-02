9435b217b2bca7a8d679060aab1d2b92113e1902
Odyssey <77610388+Odyssey409@users.noreply.github.com>
2024-08-02 22:45:47 +0900

    Revert "새로운 프로젝트를 위한 파일 분류"
    This reverts commit 714efd17f09a303188500a93ae0cdd83adc9d3dc.

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./crypto_tracker/App";
+import App from "./App";
 import { HashRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { RecoilRoot } from "recoil";
